d85540a5fc0f5685346350646a0e1da1ee15b326
João Paulo Alencar Alves <148117729+JoaoAlencar00@users.noreply.github.com>
2024-08-13 18:03:03 -0300

    #7DaysOfCode - Lógica JS 1/7: Operações Booleanas
    Primeira fase concluída :happy:

---
+++ 7daysOfcode-treinando-logica/treinando-logica/logica.js
@@ -0,0 +1,51 @@
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
+
+function separaValoresNumericos(numero, string) {
+    if (numero !== string) {
+        console.log(`${numero} é diferente de '${string}'`);
+    } else {
+        console.log(`As variáveis não tem o mesmo valor.`);
+    }
+}
+
+separaValoresNumericos(1, '1');
+separaValoresNumericos(10, '10');
+separaValoresNumericos(30, '30');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (COMPARAR O numeroUm e a stringUm) {
+//   console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+// } else {
+//   console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+// }
+
+// if (COMPARAR O numeroTrinta e a stringTrinta) {
+//   console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+// } else {
+//   console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+// }
+
+// if (COMPARAR O numeroDez e a stringDez) {
+//   console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+// } else {
+//   console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+// }
